c3b38e0c97c9369a49f1cb6d0a3e0099ae0a89d7
t4e1 <jtw5738@naver.com>
2025-08-23 04:40:43 +0900

    feat: 입력 포맷변경

---
+++ Sample.md
@@ -0,0 +1,32 @@
+# 2025.08.18
+## Work
+#### Today's work
+- N/A
+#### Issues
+- N/A
+#### Challanges
+- N/A
+#### Contribution
+- N/A
+#### Tommorow plan
+- N/A
+---
+## Personal Study 
+#### Skills
+- N/A
+#### Project 
+- N/A
+---
+## Ideas
+- N/A
+---
+## Daily Review
+#### Checklist
+- [ ] __Did study today__?
+- [ ] **Did exercise today**?
+- [ ] **Did review previous notes**? 
+#### Review
+```
+Free review for today
+```
+---
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
+	"time"
 )
 
 const (
@@ -13,20 +15,41 @@ const (
 
 func main() {
 	dirName, fileName := inputDate()
+	fmt.Println("Directory name:", dirName)
 
-	outputPath := strings.Join([]string{"./", strings.TrimSpace(dirName)}, "")
+	outputPath := strings.Join([]string{"./", dirName}, "")
+	fmt.Println("Output path:", outputPath)
+
 	createDirectory(outputPath)
-	createFile(outputPath, fileName)
+	
+	createFile(dirName, fileName)
 }
 
 func inputDate() (string, string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("What year is this year? (4-digit number (ex. 2025): ")
-	year, _ := reader.ReadString('\n')
-	fmt.Print("What month is this? (3-digit month (ex. JAN): ")
-	month, _ := reader.ReadString('\n')
+	var year int
+	var month string
+	
+	fmt.Print("Enter year and month to create directory & file (ex. 2025 JAN, 2025 FEB): ")
+	fmt.Scanln(&year, &month)
 
-	return year, month
+	if year < 0 {
+		fmt.Println("Invalid input(negative value). Please enter a valid year.")
+		return inputDate() 
+	}
+
+	if month == "" {
+		fmt.Println("Invalid input(empty value). Please enter a valid month.")
+		return inputDate() 
+	}
+
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter year.month to create path (ex. 2025.01, 2025.12, ...): ")
+		
+	// year, _ := reader.ReadString('\n')
+	// fmt.Print("What month is this? (3-digit month (ex. JAN): ")
+	// month, _ := reader.ReadString('\n')
+
+	return strconv.Itoa(year), month
 }
 
 func createDirectory(path string) error {
@@ -37,6 +60,31 @@ func createDirectory(path string) error {
 		}
 	}
 	return nil
+}
+
+func createFile(dirName, fileName string) {
+
+	filePath := strings.Join([]string{"./", dirName, "/", fileName, ".md"}, "")
+	fmt.Println("File path:", filePath)
+
+	if !existCheck(filePath) {
+		file, err := os.Create(filePath)
+		if err != nil {
+			fmt.Printf("Error creating file: %v\n", err)
+			return
+		}
+		defer file.Close()
+		
+
+		writer := bufio.NewWriter(file)
+		sampleContent, _ := readSampleFile(sampleFilePath)
+		// // 여기서 다시 시작 : for 반복문 써서 fileName에 해당하는 월의 최대일 수 만큼 반복으로 sampleCOntent 내보내기
+		// writer.WriteString(sampleContent)
+		// writer.Flush()
+		// fmt.Printf("File created successfully at %s\n", filePath)
+	} else {
+		fmt.Printf("File already exists at %s\n", filePath)
+	}
 }
 
 // Check if the directory or file exists (exist: true, not exist: false)
@@ -58,25 +106,4 @@ func readSampleFile(filepath string) (string, error) {
 	return string(data), nil
 }
 
-func createFile(path, fileName string) {
 
-	filePath := strings.Join([]string{path, "/", fileName, ".md"}, "")
-
-	if !existCheck(filePath) {
-		file, err := os.Create(filePath)
-		if err != nil {
-			fmt.Printf("Error creating file: %v\n", err)
-			return
-		}
-		defer file.Close()
-
-		writer := bufio.NewWriter(file)
-		sampleContent, _ := readSampleFile(sampleFilePath)
-		// 여기서 다시 시작 : for 반복문 써서 fileName에 해당하는 월의 최대일 수 만큼 반복으로 sampleCOntent 내보내기
-		writer.WriteString(sampleContent)
-		writer.Flush()
-		fmt.Printf("File created successfully at %s\n", filePath)
-	} else {
-		fmt.Printf("File already exists at %s\n", filePath)
-	}
-}
